bfabdb2c69b77219e4ac9c65fb48a42bb1664e4c
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2024-08-21 21:43:45 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             By{" "}
             <Image
               src="/vercel.svg"
-              alt="Vercel Logo"
+              alt="Vercel Logo" 
               className="dark:invert"
               width={100}
               height={24}
